086447417252b3007146476ae088857eb385f606
Alan <alan.ryan@jet.com>
2020-08-03 00:42:27 +0100

    dir structure changes

--- src/main.rs
+++ src/main.rs
@@ -1,169 +1,234 @@
+#[macro_use]
+extern crate slog;
+#[macro_use]
+extern crate lazy_static;
+
+extern crate google_drive3 as drive3;
 extern crate hyper;
 extern crate hyper_rustls;
-extern crate yup_oauth2 as oauth2;
-extern crate google_drive3 as drive3;
-extern crate tempfile;
 extern crate notify;
-use notify::{Watcher, RecursiveMode, watcher};
-use std::sync::mpsc::channel;
-use std::path::Path;
-use drive3::{
-    Result, 
-    Error, 
-    File, 
-    Comment, 
-    DriveHub
+extern crate tempfile;
+extern crate yup_oauth2 as oauth2;
+
+use clap::{App, Arg, SubCommand};
+use drive3::{Comment, DriveHub, Error, File, Result};
+use notify::{watcher, RecursiveMode, Watcher};
+use oauth2::{
+    parse_application_secret, read_application_secret, ApplicationSecret, Authenticator,
+    DefaultAuthenticatorDelegate, DiskTokenStorage, MemoryStorage,
 };
+use std::default::Default;
+use std::path::Path;
+use std::sync::mpsc::channel;
 use std::time::Duration;
 use tempfile::tempfile;
-use std::default::Default;
-use oauth2::{Authenticator, DefaultAuthenticatorDelegate, ApplicationSecret, MemoryStorage,  read_application_secret, parse_application_secret ,DiskTokenStorage};
+mod common;
+use slog::{Fuse, Logger};
 
-type Hub =  drive3::DriveHub<hyper::Client, oauth2::Authenticator<oauth2::DefaultAuthenticatorDelegate, oauth2::DiskTokenStorage, hyper::Client>>;
+type Hub = drive3::DriveHub<
+    hyper::Client,
+    oauth2::Authenticator<
+        oauth2::DefaultAuthenticatorDelegate,
+        oauth2::DiskTokenStorage,
+        hyper::Client,
+    >,
+>;
 
-
+lazy_static! {
+    pub static ref log: slog::Logger = Logger::root(
+        Fuse(common::PrintlnDrain),
+        o!("version" => "1", "app" => " Rusty Cam Syncer"),
+    );
+}
 
 fn read_client_secret(file: String) -> ApplicationSecret {
     read_application_secret(std::path::Path::new(&file)).unwrap()
-
 }
 
-const CLIENT_SECRET_FILE: &'static str = "/home/alan/.google-service-cli/drive3-secret.json";
-
 fn main() {
-    println!("Hello, world!");
+    info!(log, "Statring Syncer");
 
-    let secret = read_client_secret(CLIENT_SECRET_FILE.to_string());
+    let matches = App::new("Rusty Cam Syncer")
+        .version("1.0")
+        .author("Alan R. <alan@alanryan.name.com>")
+        .version("1.0")
+        .about("Will Sync a Dir recursvively with the smarts of a sheep")
+        .arg(
+            Arg::with_name("secret_file")
+                .short("s")
+                .long("secret_file")
+                .value_name("secret_file")
+                .help("Where to find Google Drive API JSON secrets")
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("target_dir")
+                .short("t")
+                .long("target_dir")
+                .value_name("target_dir")
+                .help("Directory to monitor and sync")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let secret_file = matches
+        .value_of("secret_file")
+        .unwrap_or("/home/alan/.google-service-cli/drive3-secret.json");
+
+    let target_dir = matches
+        .value_of("target_dir")
+        .unwrap_or("/tmp/pi_sync/images/");
+
+    info!(log, "Using {} as WPS Script", secret_file);
+    info!(log, "Using {} as Dir to monitoe", target_dir);
+
+    let secret = read_client_secret(secret_file.to_string());
 
     let token_storage = DiskTokenStorage::new(&String::from("temp_token")).unwrap();
-    let mut auth = Authenticator::new(&secret, DefaultAuthenticatorDelegate,
-        hyper::Client::with_connector(hyper::net::HttpsConnector::new(hyper_rustls::TlsClient::new())),
-        token_storage, Some(yup_oauth2::FlowType::InstalledInteractive));
+    let mut auth = Authenticator::new(
+        &secret,
+        DefaultAuthenticatorDelegate,
+        hyper::Client::with_connector(hyper::net::HttpsConnector::new(
+            hyper_rustls::TlsClient::new(),
+        )),
+        token_storage,
+        Some(yup_oauth2::FlowType::InstalledInteractive),
+    );
 
-    let mut hub = DriveHub::new(hyper::Client::with_connector(hyper::net::HttpsConnector::new(hyper_rustls::TlsClient::new())), auth);
+    let mut hub = DriveHub::new(
+        hyper::Client::with_connector(hyper::net::HttpsConnector::new(
+            hyper_rustls::TlsClient::new(),
+        )),
+        auth,
+    );
 
-    // Values shown here are possibly random and not representative !
-let result = hub.files().list()
-             .doit();
+    //TODO: first run
+    mkdir(&hub, "RpiCamSyncer");
 
-    println!("{:?}", result);
-
-
-    // Create a channel to receive the events.
     let (sender, receiver) = channel();
-
-    // Create a watcher object, delivering debounced events.
-    // The notification back-end is selected based on the platform.
     let mut watcher = watcher(sender, Duration::from_secs(10)).unwrap();
-
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
-    watcher.watch("/tmp/camera", RecursiveMode::Recursive).unwrap();
+    watcher.watch(target_dir, RecursiveMode::Recursive).unwrap();
 
     loop {
         match receiver.recv() {
             Ok(event) => {
                 match event {
                     notify::DebouncedEvent::Create(p) => {
-                        println!("{:?}",p);
+                        info!(log, "Path to Ceate {:?}", p);
                         let path = p.to_str().unwrap();
                         if std::path::Path::new(path).is_dir() {
                             mkdir(&hub, path);
-                        }else{
+                        } else {
                             upload(&hub, path);
                         }
-
-
                     }
 
-                    _ =>  println!("err"),
+                    _ => info!(log, "err"),
                 };
-                //println!("{:?}", event);
             }
-            Err(e) => println!("watch error: {:?}", e),
+            Err(e) => info!(log, "watch error: {:?}", e),
         }
     }
 }
 
+fn get_folder_id(hub: &Hub, path: &str) -> std::result::Result<String, String> {
+    let result = hub
+        .files()
+        .list()
+        .corpora("user")
+        .q("mimeType = 'application/vnd.google-apps.folder'")
+        .q(format!("name='{}'", path).as_str())
+        .doit();
 
+    info!(log, "{:?}", result);
 
-fn upload(hub: &Hub,  path: &str)  -> std::result::Result<u16, String> {
+    Ok("1BYD5U5oC6AY19cBmGW6JC6S6pCLZnRsG".to_owned())
+}
+
+fn upload(hub: &Hub, path: &str) -> std::result::Result<u16, String> {
     let file_path = std::path::Path::new(path);
     let mut req = drive3::File::default();
-    req.name= Some(file_path.file_name().unwrap().to_str().unwrap().to_string());
+    req.name = Some(file_path.file_name().unwrap().to_str().unwrap().to_string());
+    req.parents = Some(vec![get_folder_id(&hub, "").unwrap()]);
 
     // Values shown here are possibly random and not representative !
-    let result = hub.files().create(req)
+    let result = hub
+        .files()
+        .create(req)
         .use_content_as_indexable_text(true)
         .supports_team_drives(false)
         .supports_all_drives(true)
         .keep_revision_forever(false)
         .ignore_default_visibility(true)
         .enforce_single_parent(true)
-        .upload_resumable(std::fs::File::open(file_path.to_str().unwrap()).unwrap(), "application/octet-stream".parse().unwrap());
+        .upload_resumable(
+            std::fs::File::open(file_path.to_str().unwrap()).unwrap(),
+            "application/octet-stream".parse().unwrap(),
+        );
 
     match result {
         Err(e) => match e {
             // The Error enum provides details about what exactly happened.
             // You can also just use its `Debug`, `Display` or `Error` traits
             Error::HttpError(_)
-                |Error::MissingAPIKey
-                |Error::MissingToken(_)
-                |Error::Cancelled
-                |Error::UploadSizeLimitExceeded(_, _)
-                |Error::Failure(_)
-                |Error::BadRequest(_)
-                |Error::FieldClash(_)
-                |Error::JsonDecodeError(_, _) => {
-                    println!("{}", e);
-                    Err(e.to_string())
-                }
+            | Error::MissingAPIKey
+            | Error::MissingToken(_)
+            | Error::Cancelled
+            | Error::UploadSizeLimitExceeded(_, _)
+            | Error::Failure(_)
+            | Error::BadRequest(_)
+            | Error::FieldClash(_)
+            | Error::JsonDecodeError(_, _) => {
+                println!("{}", e);
+                Err(e.to_string())
+            }
         },
         Ok(res) => {
             println!("Success: {:?}", res);
             Ok(res.0.status.to_u16())
         }
     }
-
-
 }
 
 fn mkdir(hub: &Hub, path: &str) -> std::result::Result<u16, String> {
     let dir_path = std::path::Path::new(path);
 
+    info!(log, "Dir to create: {:?}", dir_path);
+
     let mut file = tempfile().unwrap();
     let mut req = drive3::File::default();
+    req.parents = Some(vec![get_folder_id(&hub, "").unwrap()]);
 
     req.mime_type = Some("application/vnd.google-apps.folder".to_string());
-    req.name= Some(format!("{}/{}", "camera", dir_path.file_name().unwrap().to_str().unwrap().to_string()));
+    req.name = Some(dir_path.file_name().unwrap().to_str().unwrap().to_string());
 
     // Values shown here are possibly random and not representative !
-    let result = hub.files().create(req).upload(file, "application/vnd.google-apps.folder".parse().unwrap());
+    let result = hub
+        .files()
+        .create(req)
+        .upload(file, "application/vnd.google-apps.folder".parse().unwrap());
 
     match result {
         Err(e) => match e {
             // The Error enum provides details about what exactly happened.
             // You can also just use its `Debug`, `Display` or `Error` traits
             Error::HttpError(_)
-                |Error::MissingAPIKey
-                |Error::MissingToken(_)
-                |Error::Cancelled
-                |Error::UploadSizeLimitExceeded(_, _)
-                |Error::Failure(_)
-                |Error::BadRequest(_)
-                |Error::FieldClash(_)
-                |Error::JsonDecodeError(_, _) => {
-                    println!("{}", e);
+            | Error::MissingAPIKey
+            | Error::MissingToken(_)
+            | Error::Cancelled
+            | Error::UploadSizeLimitExceeded(_, _)
+            | Error::Failure(_)
+            | Error::BadRequest(_)
+            | Error::FieldClash(_)
+            | Error::JsonDecodeError(_, _) => {
+                println!("{}", e);
 
-                    Err(e.to_string())
-                }
+                Err(e.to_string())
+            }
         },
         Ok(res) => {
             println!("Success: {:?}", res);
             Ok(res.0.status.to_u16())
         }
     }
-
 }
-
-
